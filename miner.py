655bf4ac5ab49b8ed0830b7faed019ed0c9b0bcf
pr9njamdar <79560640+pr9njamdar@users.noreply.github.com>
2022-01-07 21:30:52 +0530

    Create miner.py

---
+++ miner.py
@@ -0,0 +1,91 @@
+#@title **STEP 1 CREATE USER**
+ 
+import os
+ 
+username = "user" #@param {type:"string"}
+password = "root" #@param {type:"string"}
+ 
+print("Creating User and Setting it up")
+ 
+# Creation of user
+os.system(f"useradd -m {username}")
+ 
+# Add user to sudo group
+os.system(f"adduser {username} sudo")
+    
+# Set password of user to 'root'
+os.system(f"echo '{username}:{password}' | sudo chpasswd")
+ 
+# Change default shell from sh to bash
+os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
+ 
+print("User Created and Configured")
+
+
+#@title **INSTALL YOUR VPS BY GOD MINER**
+#@markdown  It takes 4-5 minutes for installation
+ 
+import os
+import subprocess
+ 
+#@markdown  Visit http://remotedesktop.google.com/headless and Copy the command after authentication
+ 
+CRP = "DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=\"4/0AX4XfWg20T1E-EwYsp2p3nY8zeOSZh1LMi0HkSB8DELzFdnrzQ8ZJgGxHzy-FV6l2bDdsw\" --redirect-url=\"https://remotedesktop.google.com/_/oauthredirect\" --name=$(hostname)" #@param {type:"string"}
+ 
+#@markdown Enter a pin more or equal to 6 digits
+Pin = 123456 #@param {type: "integer"}
+ 
+ 
+class CRD:
+    def __init__(self):
+        os.system("apt update")
+        self.installCRD()
+        self.installDesktopEnvironment()
+        self.installGoogleChorme()
+        self.finish()
+ 
+    @staticmethod
+    def installCRD():
+        print("Installing Chrome Remote Desktop")
+        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
+ 
+    @staticmethod
+    def installDesktopEnvironment():
+        print("Installing Desktop Environment")
+        os.system("export DEBIAN_FRONTEND=noninteractive")
+        os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
+        os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
+        os.system("apt remove --assume-yes gnome-terminal")
+        os.system("apt install --assume-yes xscreensaver")
+        os.system("systemctl disable lightdm.service")
+ 
+    @staticmethod
+    def installGoogleChorme():
+        print("Installing Google Chrome")
+        subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+        subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
+ 
+    @staticmethod
+    def finish():
+        print("Finalizing")
+        os.system(f"adduser {username} chrome-remote-desktop")
+        command = f"{CRP} --pin={Pin}"
+        os.system(f"su - {username} -c '{command}'")
+        os.system("service chrome-remote-desktop start")
+        print("Finished Succesfully")
+ 
+ 
+try:
+    if username:
+        if CRP == "":
+            print("Please enter authcode from the given link")
+        elif len(str(Pin)) < 6:
+            print("Enter a pin more or equal to 6 digits")
+        else:
+            CRD()
+except NameError as e:
+    print("username variable not found")
+    print("Create a User First")
